c325023045d83868eff965f0dae400c1c3852c18
Kevin <kzong@stuy.edu>
2019-02-08 10:53:17 -0500

    Fixed add/remove so so that add cannot add queen on an unavailable space and remove can't remove a queen that isn't there

--- QueenBoard.java
+++ QueenBoard.java
@@ -7,68 +7,80 @@ public class QueenBoard{
   }
 
   private boolean addQueen(int r, int c){
-    board[r][c] = -1;
-    for (int x = c+1; x < board[r].length; x++){
-      board[r][x] += 1;
+    if (board[r][c] != 0){
+      return false;
     }
-    for (int i = 1; r+i < board.length && c+i < board[0].length; i++ ){
-      board[r+i][c+i] += 1;
+    else{
+      board[r][c] = -1;
+      for (int x = c+1; x < board[r].length; x++){
+        board[r][x] += 1;
+      }
+      for (int i = 1; r+i < board.length && c+i < board[0].length; i++ ){
+        board[r+i][c+i] += 1;
+      }
+      for (int i = 1; r-i >= 0 && c+i < board.length; i++ ){
+        board[r-i][c+i] += 1;
+      }
+      return true;
     }
-    for (int i = 1; r-i >= 0 && c+i < board.length; i++ ){
-      board[r-i][c+i] += 1;
-    }
-    return true;
   }
 
   private boolean removeQueen(int r, int c){
-    board[r][c] = 0;
-    for (int x = c+1; x < board[r].length; x++){
-      board[r][x] -= 1;
+    if (board[r][c] != -1) return false;
+    else{
+      board[r][c] = 0;
+      for (int x = c+1; x < board[r].length; x++){
+        board[r][x] -= 1;
+      }
+      for (int i = 1; r+i < board.length && c+i < board[0].length; i++ ){
+        board[r+i][c+i] -= 1;
+      }
+      for (int i = 1; r-i >= 0 && c+i < board.length; i++ ){
+        board[r-i][c+i] -= 1;
+      }
+      return true;
     }
-    for (int i = 1; r+i < board.length && c+i < board[0].length; i++ ){
-      board[r+i][c+i] -= 1;
-    }
-    for (int i = 1; r-i >= 0 && c+i < board.length; i++ ){
-      board[r-i][c+i] -= 1;
-    }
-    return true;
   }
 
 
 
-  public String toString(){
-    String s = "";
-    for (int i = 0; i < board.length; i++){
-      s += "\n";
-      for (int x = 0; x < board[i].length; x++){
-        if (board[i][x] == -1){
-          s += "Q ";
-        }
-        else{
-          if (board[i][x] != 0){
-            s = s + board[i][x] + " ";
+    public String toString(){
+      String s = "";
+      for (int i = 0; i < board.length; i++){
+        s += "\n";
+        for (int x = 0; x < board[i].length; x++){
+          if (board[i][x] == -1){
+            s += "Q ";
           }
           else{
-            s+= "_ ";
+            if (board[i][x] != 0){
+              s = s + board[i][x] + " ";
+            }
+            else{
+              s+= "_ ";
+            }
           }
         }
       }
+      s += "\n";
+      return s;
     }
-    s += "\n";
-    return s;
+
+    public static void main(String[] args){
+      QueenBoard test = new QueenBoard(4);
+      test.addQueen(1, 0);
+      System.out.println(test);
+      System.out.println(test.addQueen(0,1));
+      System.out.println(test);
+      test.addQueen(3, 1);
+      System.out.println(test);
+      System.out.println(test.removeQueen(2,1));
+      System.out.println(test);
+      test.removeQueen(1, 0);
+      System.out.println(test);
+
+    }
+
+
+
   }
-
-  public static void main(String[] args){
-    QueenBoard test = new QueenBoard(4);
-    test.addQueen(1, 0);
-    System.out.println(test);
-    test.addQueen(3, 1);
-    System.out.println(test);
-    test.removeQueen(1, 0);
-    System.out.println(test);
-
-  }
-
-
-
-}
